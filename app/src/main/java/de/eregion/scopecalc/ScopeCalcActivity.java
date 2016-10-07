a8d0b696e79c1ec648cc25d4bc109aa419ad756e
Mathias Homann <Mathias.Homann@openSUSE.org>
2016-10-07 12:13:58 +0200

    Initial commit

---
+++ app/src/main/java/de/eregion/scopecalc/ScopeCalcActivity.java
@@ -0,0 +1,13 @@
+package de.eregion.scopecalc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScopeCalcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scope_calc);
+    }
+}
