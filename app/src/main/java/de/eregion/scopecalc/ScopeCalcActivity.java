8fbb98b6d7c9a62a534bcc735aeea4a0f8383400
Mathias Homann <Mathias.Homann@openSUSE.org>
2017-01-11 20:32:10 +0100

    have to redo the layout of the screen.

--- app/src/main/java/de/eregion/scopecalc/ScopeCalcActivity.java
+++ app/src/main/java/de/eregion/scopecalc/ScopeCalcActivity.java
@@ -2,8 +2,13 @@ package de.eregion.scopecalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ScopeCalcActivity extends AppCompatActivity {
+
+    private EditText aperture_edit, focallength_edit;
+
+    private int mmin, mmax, epmin, epmax;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
